c32591d767e67286cb12fa3ba2d77b08c9499541
Khelthyr <cedric.nils.vincendeau@gmail.com>
2020-12-02 11:36:40 +0100

    Added BDD / Templates / Static | Modified settings.py

--- TP_Django/todo/models.py
+++ TP_Django/todo/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=200)
+    is_done = models.BooleanField()
+    created_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.content
+
+    def was_published_at(self):
+        return self.created_date >= timezone.now() - datetime.timedelta(days=1)
